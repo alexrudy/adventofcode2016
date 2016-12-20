4d5706f65c0d59e1d1c1352bcade58740e049102
Alex Rudy <alex.rudy@gmail.com>
2016-12-19 22:55:28 -0800

    Day 16 complete

--- day16.py
+++ day16.py
@@ -6,24 +6,47 @@ import itertools
 
 trans = str.maketrans("01","10")
 
-def bitflip(s):
+def bitflipr(s):
     """Bitflip a string."""
-    return s.translate(trans)
+    return s.translate(trans)[::-1]
 
 def dragon(a):
     """Dragonify"""
     yield from a
     yield "0"
-    yield from bitflip("".join(reversed(a)))
+    yield from bitflipr("".join(a))
 
 def dragon_recursive(a):
     """Infinitely recurse through a dragon curve."""
     yield from a
     for i in itertools.count():
         yield "0"
-        b = bitflip("".join(reversed(a)))
+        b = bitflipr("".join(a))
         yield from b
         a = a + "0" + b
+        
+def dragon_generator(seed, steps, reverse=True):
+    """The full dragon generator."""
+    if steps == 0:
+        if reverse:
+            yield from bitflipr(seed)
+        else:
+            yield from seed
+    else:
+        yield from dragon_generator(seed, steps - 1, reverse=False)
+        yield "1" if reverse else "0"
+        yield from dragon_generator(seed, steps - 1, reverse=True)
+
+def dragon_infinite(seed):
+    """Infinite dragon generator."""
+    yield from seed
+    for i in itertools.count():
+        yield "0"
+        yield from dragon_generator(seed, i, reverse=True)
+
+def dragon_finite(seed, length):
+    """Make a finite length dragon generator"""
+    return itertools.islice(dragon_infinite(seed), length)
 
 @pytest.mark.parametrize("source, dragonified",[
     ("1", "100"),
@@ -37,12 +60,15 @@ def test_dragon(source, dragonified):
 
 def dragon_data(seed, length):
     """Generate data up to a certain length."""
-    return "".join(itertools.islice(dragon_recursive(seed), length))
+    return "".join(dragon_finite(seed, length))
 
 def iterpairs(s):
     """Iterate through pairs"""
-    for i in range(len(s) // 2):
-        yield s[i*2:i*2+2]
+    iterator = iter(s)
+    while True:
+        yield next(iterator), next(iterator)
+        
+
 
 def checksumpairs(s):
     """For a string, checksum the pairs."""
@@ -70,6 +96,31 @@ def checksum(data):
         check = "".join(checksumpairs(check))
     return check
     
+def checksum_depth(length):
+    """Given a length, how many times must we checksum to get an odd output."""
+    for i in itertools.count():
+        if length % 2 == 0:
+            length = length // 2
+        else:
+            return i - 1
+
+def checksum_base(checksum):
+    """Recursive checksum."""
+    for a,b in iterpairs(checksum):
+        yield "1" if a == b else "0"
+    
+def checksum_recursive(checksum, depth):
+    """Recursive checksum"""
+    if depth == 0:
+        yield from checksum_base(checksum)
+    else:
+        yield from checksum_recursive(checksum_base(checksum), depth - 1)
+
+def checksum_iterative(data, length):
+    """Checksum iteratively over data."""
+    depth = checksum_depth(length)
+    return checksum_recursive(data, depth)
+    
 def test_checksum():
     """Test the full algorithm."""
     assert checksum("110010110100") == "100"
@@ -80,15 +131,22 @@ def test_example():
     assert data == "10000011110010000111"
     assert checksum(data) == "01100"
     
+def test_puzzle1():
+    """Test for puzzle 1"""
+    length = 272
+    assert "".join(checksum_iterative(dragon_data(INPUT, length), length)) == "10010010110011010"
+    
 INPUT = "01000100010010111"
 def puzzle1():
     print("Puzzle #1")
-    cs = checksum(dragon_data(INPUT, 272))
+    length = 272
+    cs = "".join(checksum_iterative(dragon_data(INPUT, length), length))
     print(f"Checksum = {cs} ({len(cs)})")
     
 def puzzle2():
     print("Puzzle #2")
-    cs = checksum(dragon_data(INPUT, 35651584))
+    length = 35651584
+    cs = "".join(checksum_iterative(dragon_data(INPUT, length), length))
     print(f"Checksum = {cs} ({len(cs)})")
     
 if __name__ == '__main__':
